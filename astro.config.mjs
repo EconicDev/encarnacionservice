76513aeb1ab9fc6e0db9674498090aa9f9cdaa75
Robert Abreu <robert.abreu3@gmail.com>
2026-04-09 20:47:53 -0400

    upgrade astro packages v6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import react from "@astrojs/react";
 
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), react(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  integrations: [astroI18next(), react()],
   site: "https://econicdev.github.io/",
   base: "/"
 });
